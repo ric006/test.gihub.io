86c6f54b4945ea81e6009caa1076e3785f56f0a4
ric006 <70054337+ric006@users.noreply.github.com>
2024-02-12 23:52:45 -0600

    Create index.tsx

---
+++ index.tsx
@@ -0,0 +1,11 @@
+// index.tsx
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Import your root component
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
